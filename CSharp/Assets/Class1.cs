0748c8eb84f9c3a7e8d9633b2fdfdc484c040931
kat20205 <63327503+kat20205@users.noreply.github.com>
2020-04-08 15:32:17 +0800

    認識欄位

--- CSharp/Assets/Class1.cs
+++ CSharp/Assets/Class1.cs
@@ -10,6 +10,11 @@ public class Class1 : MonoBehaviour
     // C#
     //1. 大小不一樣
     //2.括號成對出現() [] {} '' "" < >
+    
+    [Header("汽車1號")]
+    public CAR CAR1;
+    [Header("汽車2號")]
+    public CAR CAR2;
 
     //事件:在特定時間會以指定次數執行的方法(名稱為藍色)
     //開始事件:播放遊戲時執行一次-初始設定
@@ -17,8 +22,18 @@ public class Class1 : MonoBehaviour
     {
         //輸出("文字訊息");
         //C#結尾為分號;
+        print("哈囉、沃德~");
 
-        print("哈囉沃德~");
+        //取得與設定欄位
+        //取得 get
+        print("汽車1號的 CC數;"  + CAR1.cc);
+        print("汽車2號的品牌;"   + CAR2.brand);
+
+        //設定 set
+        CAR1.cc = 9999;
+        CAR1.weight = 999.9f;
+        CAR1.brand = "BMW";
+        CAR1.brake = true;
     }
 }
 
